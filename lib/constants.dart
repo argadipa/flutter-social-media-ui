00bd6f086b08bcd87e054458259dafd5746ca6f2
argadipame <argadipa.pw@mecreativeid.com>
2020-05-12 12:37:50 +0700

    Add TabBar to Home Screen.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,6 @@
 import 'package:flutter/material.dart';
+
+const kTabTextStyle = TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.w600,
+);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -7,13 +8,26 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
+          brightness: Brightness.light,
+          iconTheme: IconThemeData(
+            color: Theme.of(context).primaryColor,
+          ),
           title: Text(
             'STREAM',
             style: TextStyle(
@@ -21,6 +35,23 @@ class _HomeScreenState extends State<HomeScreen> {
               fontWeight: FontWeight.bold,
               letterSpacing: 10.0,
             ),
+          ),
+          bottom: TabBar(
+            controller: _tabController,
+            labelColor: Theme.of(context).primaryColor,
+            labelStyle: kTabTextStyle,
+            unselectedLabelStyle: TextStyle(
+              fontSize: 18.0,
+            ),
+            indicatorWeight: 3.0,
+            tabs: <Widget>[
+              Tab(
+                text: 'Trending',
+              ),
+              Tab(
+                text: 'Latest',
+              ),
+            ],
           ),
         ),
       ),
